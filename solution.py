17740d4769af543cd1d6773bd90fcdd175a06393
Cameron Boy <boyxx001@gmail.com>
2017-09-14 18:28:21 -0600

    Cross Product Function added

--- solution.py
+++ solution.py
@@ -29,7 +29,7 @@ def naked_twins(values):
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    pass
+    return [s+t for s in A for t in B]
 # TODO: cross
 
 def grid_values(grid):
