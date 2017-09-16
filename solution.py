3f1ca79adeb8c88feb6669cbad98d778cf051200
Cameron Boy <camerondavidboy@gmail.com>
2017-09-15 22:49:37 -0600

    Some further function coding along with a colon to cap off a loop.

--- solution.py
+++ solution.py
@@ -24,8 +24,27 @@ def naked_twins(values):
         the values dictionary with the naked twins eliminated from peers.
     """
     # TODO: Naked Twins
-    # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
+
+    #Getting the potential twins, those boxes wtih two values
+    candidates = [box for box in values.keys() if len(value[box]) == 2]
+
+
+    naked_twins = [[box1, box2] for box1 in potential_twins \
+                                for box2 in peers[box1] \
+                                if set(values[box1]) == set(values[box2])]
+
+    for i in range(len(naked_twins)):
+        box1 = naked_twins[i][0]
+        box2 = naked_twins[i][1]
+        peers1 = set(peers[box1])
+        peers2 = set(peers[box2])
+        peers_int = peers1 & peers2
+        for peer_val in peers_int:
+            if len(values[peer_val])>2:
+                for rm_val in values[box1]:
+                    #values[peer_val] = values[peer_val].replace(rm_val,'')
+                    values = assign_value(values, peer_val, values[peer_val].replace(rm_val,''))
+    return values
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
@@ -68,18 +87,23 @@ def display(values):
 
 
 def eliminate(values):
+    """
+    When a box is already solved, i.e. only has one value, then we can and
+    should remove that value from it's peers
+    """
     solves_values = [box for box in values.keys() if len(values[box]) ==1]
 
     for box in solved_values:
         digit = values[box]
-        for peer in peers[box]
+        for peer in peers[box]:
             values[peer] = values[peer].replace(digit, '')
     return values
 
 
 def only_choice(values):
     """
-
+    Assign a box a value that only appears as an option for a certain box
+    among a set of peers
     """
     for unit in unitlist:
         for digit in '123456789'
@@ -89,11 +113,50 @@ def only_choice(values):
     return values
 
 def reduce_puzzle(values):
-    pass
-    # TODO: reduce_puzzle
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        values = naked_twins(values)
+
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+
 def search(values):
-    pass
-# TODO: search
+    """
+    Using depth-first search and propagation, try all possible values.
+    """
+
+    #First we want ot reduce the puzzle wtih our previous methods.
+    values = reduce_puzzle(values)
+    if values is False:
+        return False # The puzzle previously Failed
+    if all(len(values[s]) == 1 for s in boxes):
+        return values #The puzzle is solved! ALL boxes have one values
+
+    #Now we must choose oneo fthe unfilled squares with the fewest possibilities
+    #This will be our starting point for our search tree.
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    #Now use recurrence to solve each of the resulting sudokus
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
